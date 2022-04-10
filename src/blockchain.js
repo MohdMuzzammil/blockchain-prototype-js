9ddafe806e17a4bab0f01a6175950bdcc04f4256
Mohammed Muzzammil <muzzammilmohammed32@gmail.com>
2022-04-10 20:10:55 +0530

    Added new block method

--- src/blockchain.js
+++ src/blockchain.js
@@ -3,4 +3,18 @@ function Blockchain() {
   this.newTransactions = [];
 }
 
- 
+Blockchain.prototype.createNewBlock = (nonce, previousBlockHash, hash) => {
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.newTransactions,
+    nonce: nonce,
+    hash: hash,
+    previousBlockHash: previousBlockHash,
+  };
+  this.newTransactions = [];
+  this.chain.push(newBlock);
+  return newBlock;
+};
+
+module.exports = Blockchain;
